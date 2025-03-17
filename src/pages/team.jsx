b4f488ab6b97624f1e67b121cea2eecf1a1e91e4
ayush kumar <ayushkumar09005@gmail.com>
2025-03-17 20:03:26 +0530

    added many sections

--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -44,7 +44,7 @@ const Team = () => {
 
         {/* left section */}
         <motion.div
-          initial={{ opacity: 0, y: 30 }}
+          initial={{ opacity: 0, y: isMobile ? 20 : 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.2, ease: "easeInOut"}}
         >
@@ -55,7 +55,7 @@ const Team = () => {
         </motion.div>
         {/* right section */}
         <motion.div
-          initial={{ opacity: 0, y: 30 }}
+          initial={{ opacity: 0, y: isMobile ? 20 : 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.2 , ease: "easeInOut"}}
         >
